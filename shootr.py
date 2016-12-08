527904bfe0a6f9b1a20f4bd1c7b252d23c5490be
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2016-12-08 11:19:53 +0100

    tidied things

--- shootr.py
+++ shootr.py
@@ -6,10 +6,11 @@ import time as sleepr
 
 def main():
     """CriminalDuty - a mini pygame for goons"""
+    # SETUP -----------------------------------------------------------------------
     black = 0, 0, 0
     white = 255, 255, 255
     screen_w, screen_h = 700, 400
-    player_x, player_y = 0, 0
+    player_x, player_y = 50, 100
     move_player_x, move_player_y = 0, 0
     num_cops = 5
     score = 0
@@ -18,7 +19,9 @@ def main():
     SONGS = [os.path.join('snd\\songs', 'Bando.mp3'), os.path.join('snd\\songs', 'KarateChop.mp3')]
     KILLWAV = [os.path.join('snd\\kill', 'Gucci Mane Yeah (2).wav'), os.path.join('snd\\kill', 'Vox (Mafia-Hoe).wav')]
     HITWAV = os.path.join('snd\\hit', 'OJ Damn.wav')
-
+    all_sprites_list, player_list, cop_list, tank_list, heli_list, bullet_list, canon_list, rocket_list = (pygame.sprite.Group() for i in range(8))
+    cop_hit_list, tank_hit_list, heli_hit_list, cop_kill_list, tank_kill_list, heli_kill_list, canon_hit_list, rocket_hit_list = ([] for i in range(8))
+    
     # HELPRS ----------------------------------------------------------------------
     def get_key():
         while 1:
@@ -45,7 +48,7 @@ def main():
         pygame.display.flip()
     # -----------------------------------------------------------------------------
     def ask(screen, question):
-        """ask(screen, question) -> answer"""
+        """ask(screen, question) returns answer"""
         pygame.font.init()
         current_string = []
         display_box(screen, question + ': ' + string.join(current_string, ''))
@@ -80,15 +83,15 @@ def main():
 
     # CLASSES ---------------------------------------------------------------------
     class Cop(pygame.sprite.Sprite):
-        """This is the Cop class so far, cop by foot chasing player."""
+        """Cop by foot chasing player"""
         def __init__(self):
-            """Give stoopid cop look"""
+            """stoopid cop; init with random coords"""
             pygame.sprite.Sprite.__init__(self)
             self.image, self.rect = load_png('cop.png')
             self.rect.x = random.randrange(screen_w)
             self.rect.y = random.randrange(330)
         def update(self):
-            """Cop movement: constantly chasing the player"""
+            """cop movement: constantly chasing the player"""
             if player_x < self.rect.x:
                 self.rect.x -= 1
             elif player_x > self.rect.x:
@@ -99,15 +102,15 @@ def main():
                 self.rect.y += 1
     # -----------------------------------------------------------------------------
     class Tank(pygame.sprite.Sprite):
-        """Tank doesnt chase player, adjusts y only and shoots player"""
+        """Tank doesnt chase player, adjusts y only and shoots player, level 1"""
         def __init__(self):
-            """Assign pic later on"""
+            """first endgegner"""
             pygame.sprite.Sprite.__init__(self)
             self.image, self.rect = load_png('tank.png')
             self.rect.x, self.rect.y = 500, 200
             self.health = 1000
         def update(self):
-            """Tank movement: adjusting y rel 2 player"""
+            """tank movement: adjusting y rel 2 player"""
             if player_y < self.rect.y:
                 self.rect.y -= 1
             elif player_y > self.rect.y:
@@ -115,7 +118,7 @@ def main():
             elif player_y == self.rect.y:
                 self.shoot()
         def shoot(self):
-            """Tank shoots player"""
+            """tank shoots player"""
             canon = Bullet([player_x, player_y], [self.rect.x, self.rect.y])
             canon.rect.x = self.rect.x
             canon.rect.y = self.rect.y
@@ -123,14 +126,14 @@ def main():
             canon_list.add(canon)
     # -----------------------------------------------------------------------------
     class Heli(pygame.sprite.Sprite):
-        """Heli flys, adjusts x only, appears when tank dead"""
+        """Heli flys, adjusts x only, appears when tank dead, level 2"""
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image, self.rect = load_png('heli.png')
             self.rect.x, self.rect.y = 500, 0
             self.health = 1500
         def update(self):
-            """Heli movement: adjusting x rel 2 player"""
+            """heli movement: adjusting x rel 2 player"""
             if player_x < self.rect.x:
                 self.rect.x -= 2
             elif player_x > self.rect.x:
@@ -138,7 +141,7 @@ def main():
             elif player_x == self.rect.x:
                 self.shoot()
         def shoot(self):
-            """Heli shoots player"""
+            """heli shoots player"""
             rocket = Bullet([player_x, player_y], [self.rect.x, self.rect.y])
             rocket.rect.x = self.rect.x
             rocket.rect.y = self.rect.y
@@ -148,36 +151,42 @@ def main():
     class Player(pygame.sprite.Sprite):
         """The Player class"""
         def __init__(self):
-            """Load in avatar"""
+            """load in avatar; could add feature to load in custom pic?"""
             pygame.sprite.Sprite.__init__(self)
             self.image, self.rect = load_png('goon.png')
-            self.rect.x, self.rect.y = 2, 2
+            self.rect.x, self.rect.y = player_x, player_y
             self.health = 100   
         def update(self):
             """player_x,_y are the players new coords for every next frame"""
             self.rect.x = player_x
             self.rect.y = player_y
+        def shoot(self):
+            bullet = Bullet(pygame.mouse.get_pos(), [player.rect.x, player.rect.y])
+            bullet.rect.x = player.rect.x
+            bullet.rect.y = player.rect.y
+            all_sprites_list.add(bullet)
+            bullet_list.add(bullet)
         def hit_vox(self):
-            """Play this when player got hit"""
+            """play this when player got hit"""
             pygame.mixer.Sound(HITWAV).play()    
         def kill_vox(self):
-            """Play this when player just killed"""
+            """play this when player just killed"""
             pygame.mixer.Sound(random.choice(KILLWAV)).play()
     # -----------------------------------------------------------------------------
     class Bullet(pygame.sprite.Sprite):
-        """Class for bullets player and tank shoot"""
+        """Class for bullets player, heli, tank.. shoot"""
         speed = 4.
-        def __init__(self, mouse, player):
-            """Give look and provide coords at init"""
+        def __init__(self, target, instance):
+            """target r x n y coords of well target; instance, well, the instnce shooting the bullet"""
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface([8, 6])
             self.image.fill(black)
-            self.mouse_x, self.mouse_y = mouse[0], mouse[1]
-            self.player = player
+            self.target_x, self.target_y = target[0], target[1]
+            self.instance = instance
             self.rect = self.image.get_rect()    
         def update(self):
-            """Bullets flying their way"""
-            distance = [self.mouse_x - self.player[0], self.mouse_y - self.player[1]]
+            """bullets flying their way"""
+            distance = [self.target_x - self.instance[0], self.target_y - self.instance[1]]
             norm = math.sqrt(distance[0] ** 2 + distance[1] ** 2)
             direction = [distance[0] / norm, distance[1] / norm]
             bullet_vector = [direction[0] * Bullet.speed, direction[1] * Bullet.speed]
@@ -187,7 +196,7 @@ def main():
     class Footr(pygame.sprite.Sprite):
         """Basically a scoreboard, how2 raise its z-value, so that no overlap??"""
         def __init__(self):
-            """Setting up the scoreboard"""
+            """setting up the scoreboard"""
             pygame.sprite.Sprite.__init__(self)
             self.font = pygame.font.Font(None, 24)
             self.text = self.font.render('{}'.format(NAME) + '    health: {}'.format(player.health) + '    cops shot: {}'.format(score) + '    tank: {}'.format(tank.health), 1, white)
@@ -196,7 +205,7 @@ def main():
             self.rect.left, self.rect.top = 0, 350
             self.image.blit(self.text, (10, 10))
         def update(self):
-            """Updating health and score"""
+            """updating health and score"""
             if len(tank_list) == 1:
                 self.text = self.font.render('{}'.format(NAME) + '    health: {}'.format(player.health) + '    cops shot: {}'.format(score) + '    tank: {}'.format(tank.health), 1, white)
             elif len(heli_list) == 1:
@@ -210,24 +219,16 @@ def main():
     pygame.mixer.pre_init(frequency = 44100, size = -16, channels = 2, buffer = 4096)
     pygame.init(); print(pygame.mixer.get_init())
     pygame.mixer.music.load(random.choice(SONGS))
+    sleepr.sleep(1)
     pygame.mixer.music.play()
     screen = pygame.display.set_mode([screen_w, screen_h])
     pygame.display.set_caption('CriminalDuty')
     NAME = ask(screen, 'Name')
-    # Level 1
+    # Level 1 ---------------------------------------------------------------------
     screen.fill(black)
     screen.blit(pygame.font.Font(None, 96).render('Level 1', 1, white), (250, 150))
     pygame.display.flip()
     sleepr.sleep(5)
-    # -----------------------------------------------------------------------------
-    all_sprites_list = pygame.sprite.Group()
-    player_list = pygame.sprite.Group()
-    cop_list = pygame.sprite.Group()
-    tank_list = pygame.sprite.Group()
-    heli_list = pygame.sprite.Group()
-    bullet_list = pygame.sprite.Group()
-    canon_list = pygame.sprite.Group()
-    rocket_list = pygame.sprite.Group()
     # -----------------------------------------------------------------------------
     for i in range(num_cops):
         cop = Cop()
@@ -249,12 +250,8 @@ def main():
                 display_box(screen, 'cops shot: {}'.format(score))
                 sleepr.sleep(10)
                 done = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:  # player shooting bullets
-                bullet = Bullet(pygame.mouse.get_pos(), [player.rect.x, player.rect.y])
-                bullet.rect.x = player.rect.x
-                bullet.rect.y = player.rect.y
-                all_sprites_list.add(bullet)
-                bullet_list.add(bullet)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                player.shoot()
             # player movement
             if event.type == pygame.KEYDOWN:    
                 if event.key == K_a:
@@ -283,22 +280,22 @@ def main():
         # new player position
         player_x += move_player_x
         player_y += move_player_y
-        # Cop instances hitting player
+        # cops hitting player
         cop_hit_list = pygame.sprite.spritecollide(player, cop_list, True)
         for cop in cop_hit_list:
             player.hit_vox()
             player.health -= 5
-        # Tank instance overlapping with player
+        # tank overlapping with player
         tank_hit_list = pygame.sprite.spritecollide(player, tank_list, False)
         for tank in tank_hit_list:
             player.hit_vox()
             player.health -= 10
-        # Heli instance overlapping with player
+        # heli overlapping with player
         heli_hit_list = pygame.sprite.spritecollide(player, heli_list, False)
         for heli in heli_hit_list:
             player.hit_vox()
             player.health -= 10
-        # Cop instances that player shot
+        # cops that player shot
         for bullet in bullet_list:
             cop_kill_list = pygame.sprite.spritecollide(bullet, cop_list, True)
             for cop in cop_kill_list:
@@ -306,13 +303,13 @@ def main():
                 bullet_list.remove(bullet)
                 all_sprites_list.remove(bullet)
                 score += 1
-            # bullets hitting Tank instance
+            # bullets hitting tank
             tank_kill_list = pygame.sprite.spritecollide(bullet, tank_list, False)
             for hit in tank_kill_list:
                 bullet_list.remove(bullet)
                 all_sprites_list.remove(bullet)
                 tank.health -= 10
-            # bullets hitting Heli instance
+            # bullets hitting heli
             heli_kill_list = pygame.sprite.spritecollide(bullet, heli_list, False)
             for hit in heli_kill_list:
                 bullet_list.remove(bullet)
@@ -323,7 +320,7 @@ def main():
                 bullet.rect.x < -10 or bullet.rect.x > screen_w - 40):
                 bullet_list.remove(bullet)
                 all_sprites_list.remove(bullet)
-        # Tanks canons aimed at player
+        # tanks canons aimed at player
         """canons out of bounds r not removed, u cant hide outta view, Tank instance will still shoot ya"""
         for canon in canon_list:
             canon_hit_list = pygame.sprite.spritecollide(canon, player_list, False)
@@ -332,7 +329,7 @@ def main():
                 canon_list.remove(canon)
                 all_sprites_list.remove(canon)
                 player.health -= 10
-        # Helis rockets aimed at player
+        # helis rockets aimed at player
         """rockets out of bounds r not removed, u cant hide outta view, Heli instance will still shoot ya"""
         for rocket in rocket_list:
             rocket_hit_list = pygame.sprite.spritecollide(rocket, player_list, False)
@@ -341,7 +338,7 @@ def main():
                 rocket_list.remove(rocket)
                 all_sprites_list.remove(rocket)
                 player.health -= 15
-        # Revive cops++ when all got shot
+        # revive cops++ when all got shot
         if len(cop_list) == 0:
             num_cops += 1
             for i in range(num_cops):
